fc531d070319b0ad7b0cc30741da13edcb90097b
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2026-04-17 17:53:15 +0200

    chore: config.hosts and health path auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,12 +115,9 @@ Rails.application.configure do
   config.middleware.delete(Rack::Runtime)
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = ["hackt.news", /.*\.hackt\.news/]
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = {exclude: ->(request) { request.path == "/up" }}
 
   config.active_storage.service = :local
 end
