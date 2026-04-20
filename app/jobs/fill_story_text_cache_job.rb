5cb2f58e2c180987a3755d5f9989855d3899b8ce
Davide Giuseppe Monaco <monaco.dg@gmail.com>
2026-04-20 18:50:20 +0200

    feat: integrate diffbot into recurring jobs

---
+++ app/jobs/fill_story_text_cache_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class FillStoryTextCacheJob < ApplicationJob
+  queue_as :default
+
+  CURSOR_KEY = "story_text:last_story_id"
+  LOOKBACK = 3.days
+
+  def perform
+    last_id = (Keystore.value_for(CURSOR_KEY) || Story.last&.id).to_i
+
+    Story
+      .where("id > ? AND is_deleted = ? AND created_at >= ?", last_id, false, LOOKBACK.ago)
+      .order(:id)
+      .each do |s|
+        StoryText.fill_cache!(s)
+        Keystore.put(CURSOR_KEY, s.id)
+      end
+  end
+end
---
+++ spec/jobs/fill_story_text_cache_job_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+RSpec.describe FillStoryTextCacheJob, type: :job do
+  let(:job) { described_class.new }
+
+  before do
+    allow(DiffBot).to receive(:get_story_text).and_return("diffbot body")
+    Keystore.where(key: described_class::CURSOR_KEY).delete_all
+  end
+
+  describe "#perform" do
+    it "creates a StoryText for each new story and advances the cursor" do
+      a = create(:story)
+      b = create(:story)
+      Keystore.put(described_class::CURSOR_KEY, a.id - 1)
+
+      job.perform
+
+      expect(StoryText.where(id: [a.id, b.id]).count).to eq(2)
+      expect(Keystore.value_for(described_class::CURSOR_KEY)).to eq(b.id)
+    end
+
+    it "skips deleted stories" do
+      deleted = create(:story, :deleted)
+      live = create(:story)
+      Keystore.put(described_class::CURSOR_KEY, deleted.id - 1)
+
+      job.perform
+
+      expect(StoryText.where(id: deleted.id)).not_to exist
+      expect(StoryText.where(id: live.id)).to exist
+      expect(Keystore.value_for(described_class::CURSOR_KEY)).to eq(live.id)
+    end
+
+    it "ignores stories older than the lookback window" do
+      old = create(:story)
+      old.update_columns(created_at: 4.days.ago)
+      Keystore.put(described_class::CURSOR_KEY, old.id - 1)
+
+      job.perform
+
+      expect(StoryText.where(id: old.id)).not_to exist
+      expect(Keystore.value_for(described_class::CURSOR_KEY)).to eq(old.id - 1)
+    end
+
+    it "starts from Story.last when the cursor is unset" do
+      earlier = create(:story)
+      StoryText.create!(id: earlier.id, title: earlier.title, description: earlier.description, body: "seeded")
+      later = create(:story)
+
+      job.perform
+
+      expect(StoryText.find(earlier.id).body).to eq("seeded")
+      expect(StoryText.where(id: later.id)).not_to exist
+    end
+  end
+end
